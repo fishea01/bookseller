012c2d8b7ddf8a7573a5952bdf1ea248c4d1410d
joeyb <joeyb@DESKTOP-BV9U0A6>
2017-09-22 06:42:13 -0500

    Rough profile pages created, when the header gets included can add the styles and make them look nice.

---
+++ profile.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <!--TODO: User image to the left?, center the container and surround with border and add styles-->
+        
+        <div class="container">
+            <h2>Profile</h2>
+            <br>
+            <p>Name : <span>Jane Doe</span></p>
+                        
+            <p>Location : <span>Franklin University</span></p>
+                      
+            <p>Email : <span>jane@email.com</span></p>
+            
+            
+            
+        </div>
+    </body>
+</html>
