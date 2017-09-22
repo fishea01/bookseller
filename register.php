ea6c7f64974dc72330c530f6333c8726180152b8
joeyb <joeyb@DESKTOP-BV9U0A6>
2017-09-22 06:11:54 -0500

    Login and Register Created

---
+++ register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h2>Register</h2>
+        <div class="container center">
+            
+            <br>
+            <label>Username</label>
+            <input type="text" placeholder="Enter Username" name="userName" required>
+            
+            <br><br>
+            <label>Password</label>
+            <input type="text" placeholder="Enter Password" name="passWord" required>
+            
+            <br><br>
+            <label>Verify Password</label>
+            <input type="text" placeholder="Verify Password" name="passWordVerify" required>
+            
+            <br><br>
+            <button type="submit">Login</button>
+            
+            
+        </div>
+    </body>
+</html>
