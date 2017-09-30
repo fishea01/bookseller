b5a4355d70a39b3b20316aea6e4ade524bb7185e
joeyb <joeyb@DESKTOP-BV9U0A6>
2017-09-30 16:37:10 -0500

    Added the Search and Sell Book pages

---
+++ sell.php
@@ -0,0 +1,35 @@
+<?php include 'header.php'; ?>
+
+
+<div class="from-group center">
+    <h2>Sell Book</h2>
+
+    <div class="container">
+
+        <br>
+        <label>Title</label>
+        <input type="text" placeholder="Book Title" name="bookTitle" required>
+
+        <br><br>
+        <label>ISBN</label>
+        <input type="text" placeholder="Enter ISBN" name="ISBN" required>
+
+        <br><br>
+        <label>Enter Price</label>
+        <input type="text" placeholder="0.00" name="price" required>
+
+        <br><br>
+        <label>Quality of Book</label>
+        <input type="text" placeholder="Enter Quality" name="quality" required>
+
+        <br><br>
+        <label>&nbsp;</label>
+        <button type="submit">Sell</button>
+        <button type="cancel">Cancel</button>
+
+        <br/><br/>
+    </div>
+
+</div>
+</body>
+</html>
