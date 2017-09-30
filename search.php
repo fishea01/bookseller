4464e123ad36bbc2122b3cd060d9f225df105e55
joeyb <joeyb@DESKTOP-BV9U0A6>
2017-09-30 16:37:17 -0500

    Added the Search and Sell Book pages

---
+++ search.php
@@ -0,0 +1,75 @@
+<?php include 'header.php'; ?>
+
+<div class="container">
+    <div class="panel panel-default">
+        <h2>Book Search</h2>
+ 
+        <!--Search in the header-->
+        <div class="panel-heading text-center clearfix">
+
+            <div class="pull-right">
+                <!--TODO: action needs to be the search results, back to the same page  GET or POST?--> 
+                <form class="form-inline input-group" action="search.php" method="POST">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="searchFor" placeholder="Book / ISBN ">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="location" placeholder="Location">
+                    </div>
+                    <button type="submit" class="btn btn-default">Search</button>
+                </form>
+            </div>
+        </div>
+        <div class="panel-body">
+
+            <!--TODO: Sorting????--> 
+            <table class="table table-striped table-hover">
+                <thead>
+                    <th>Book Title</th>
+                    <th>ISBN</th>
+                    <th>Price</th>
+                    <th>Quality</th>
+                    <th>Location</th>
+                </thead>
+                <tbody>
+
+                    <!--TODO: PHP Loop to create table entries dynamically based on results-->
+                    <tr>
+                        <td>Book Title 1</td>
+                        <td>000000000000</td>
+                        <td>$0.00</td>
+                        <td>Like New</td>
+                        <td>Franklin University</td>       
+                    </tr>
+                    <tr>
+                        <td>Book Title 2</td>
+                        <td>000000000000</td>
+                        <td>$0.00</td>
+                        <td>Good</td>
+                        <td>Franklin University</td>       
+                    </tr>
+                    <tr>
+                        <td>Book Title 3</td>
+                        <td>000000000000</td>
+                        <td>$0.00</td>
+                        <td>Bad</td>
+                        <td>Franklin University</td>       
+                    </tr>
+                    
+                </tbody>
+                
+            </table>
+
+        </div>
+
+        <!--TODO: Search results maybe?--> 
+        <div class="panel-footer text-center">
+            <p>3 records found</p>
+        </div>
+
+
+    </div>
+</div>
+
+</body>
+</html>
