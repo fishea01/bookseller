9236f31862c4d312b46f7faea7480c0cc3ba6fee
joeyb <joeyb@DESKTOP-BV9U0A6>
2017-09-30 16:56:48 -0500

    Added the Search and Sell Book pages

--- register.php
+++ register.php
@@ -4,37 +4,37 @@
 <div class="from-group center">
     <h2>Register</h2>
 
-        <div class="container">
-            
-            <br>
-            <label>Username</label>
-            <input type="text" placeholder="Enter Username" name="userName" required>
-            
-            <br><br>
-            <label>Password</label>
-            <input type="text" placeholder="Enter Password" name="passWord" required>
-            
-            <br><br>
-            <label>Verify Password</label>
-            <input type="text" placeholder="Verify Password" name="passWordVerify" required>
-            
-            <br><br>
-            <label>Location</label>
-            <input type="text" placeholder="Location" name="location" required>
-            
-            <br><br>
-            <label>Email Address</label>
-            <input type="text" placeholder="Contact Email" name="email" required>
-            
-            
-            
-            <br><br>
-            <label>&nbsp;</label>
-            <button type="submit">Register</button>
-            
-            <br/><br/>
-        </div>
-    
+    <div class="container">
+
+        <br>
+        <label>Username</label>
+        <input type="text" placeholder="Enter Username" name="userName" required>
+
+        <br><br>
+        <label>Password</label>
+        <input type="text" placeholder="Enter Password" name="passWord" required>
+
+        <br><br>
+        <label>Verify Password</label>
+        <input type="text" placeholder="Verify Password" name="passWordVerify" required>
+
+        <br><br>
+        <label>Location</label>
+        <input type="text" placeholder="Location" name="location" required>
+
+        <br><br>
+        <label>Email Address</label>
+        <input type="text" placeholder="Contact Email" name="email" required>
+
+
+
+        <br><br>
+        <label>&nbsp;</label>
+        <button type="submit">Register</button>
+
+        <br/><br/>
+    </div>
+
 </div>
-    </body>
+</body>
 </html>
